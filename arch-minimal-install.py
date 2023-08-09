eff8628b957049d64f5900b3d20ccb6d3f5eff38
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-08-09 22:30:30 +0900

    Update arch-minimal-install.py

--- arch-minimal-install.py
+++ arch-minimal-install.py
@@ -3,14 +3,15 @@ import os
 install_device = "/dev/sda"
 
 os.system('loadkeys jp106')
-os.system('lsblk')
-#os.system(f'cd {install_device}')
-
 os.system(f'fdisk {install_device}')
-os.system('o')
 
-os.system('n')
-os.system('p')
-os.system('1')
-os.system('')
-os.system('+512M')
+import SendKeys
+SendKeys.SendKeys('o')
+
+#os.system('o')
+
+#os.system('n')
+#os.system('p')
+#os.system('1')
+#os.system('')
+#os.system('+512M')
