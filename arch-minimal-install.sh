1b8aae0513bf63b6c21dbe4efaaa237a2bdb0754
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2023-08-10 00:57:04 +0900

    Update arch-minimal-install.sh

--- arch-minimal-install.sh
+++ arch-minimal-install.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
+GREEN=$(tput setaf 2; tput bold)
+function green() {
+    echo -e "$GREEN$*$NORMAL"
+}
 
 ################################################################################
 # Setting
@@ -8,13 +12,15 @@ HOST_NAME=arch
 ROOT_PASSWORD=tsuyoki
 USER_NAME=tsuyoki
 USER_PASSWORD=tsuyoki
-
-
 loadkeys jp106
+
+
 
 ################################################################################
 # Create Partitions
 ################################################################################
+green ""
+green "FDISK..."
 
 # FDISK
 fdisk $INSTALL_DEVICE << __EOF__
@@ -34,11 +40,13 @@ p
 w
 __EOF__
 
-# Format Disks
+green ""
+green "Format Disks..."
 mkfs.fat -F 32 ${INSTALL_DEVICE}1
 mkfs.ext4 ${INSTALL_DEVICE}2
 
-# Mount Disks
+green ""
+green "Mount Disks..."
 mount ${INSTALL_DEVICE}2 /mnt
 mount --mkdir ${INSTALL_DEVICE}1 /mnt/boot
 
@@ -46,22 +54,17 @@ mount --mkdir ${INSTALL_DEVICE}1 /mnt/boot
 # System Insall
 ################################################################################
 
-########################################
-# Select Mirror
+green ""
+green "Select Mirror..."
 reflector -country 'Japan' --sort rate -save /etc/pacman.d/mirrorlist
 
-#`/etc/pacman.d/mirrorlist`
-#手動で以下を追加しても良いかも？
-#echo Server = https://ftp.jaist.ac.jp/pub/Linux/ArchLinux/\$repo/os/\$arch > /etc/pacman.d/mirrorlist
-#$ は特殊文字なので、echo内で使用する場合は、バックスラッシュを使う
-#文頭に追加するにはどうするんだ？
-########################################
+green ""
+green "Base Package..."
+pacstrap -K /mnt base linux linux-firmware base-devel networkmanager intel-ucode vim
 
-# Base Package
-#pacstrap -K /mnt base linux linux-firmware base-devel networkmanager intel-ucode vim
-pacstrap -K /mnt base linux networkmanager intel-ucode vim
 
-# fstab
+green ""
+green "fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # chroot
@@ -71,29 +74,35 @@ genfstab -U /mnt >> /mnt/etc/fstab
 # ここで、止まってた（arch-chroot が原因）
 ########################################
 
-# TimeZone
+green ""
+green "TimeZone..."
 #ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
-# Harcware Clock Setting
+green ""
+green "Harcware Clock Setting..."
 #hwclock --systohc
 arch-chroot /mnt hwclock --systohc
 
-# localization
+green ""
+green "Localization..."
 #sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
 #sed -i "s/#ja_JP.UTF-8/ja_JP.UTF-8/g" /etc/locale.gen
 arch-chroot /mnt sed -i "s/#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
 arch-chroot /mnt sed -i "s/#ja_JP.UTF-8/ja_JP.UTF-8/g" /etc/locale.gen
 
-# Create locale
+green ""
+green "Create locale..."
 #locale-gen
 arch-chroot /mnt locale-gen
 
-# Create locale.conf
+green ""
+green "Create locale.conf..."
 #echo LANG=ja_JP.UTF-8 > /etc/locale.conf
 arch-chroot /mnt echo LANG=ja_JP.UTF-8 > /etc/locale.conf
 
-# Keymap Setting
+green ""
+green "Keymap Setting..."
 #echo KEYMAP=jp106 > /etc/vconsole.conf
 arch-chroot /mnt echo KEYMAP=jp106 > /etc/vconsole.conf
 
@@ -103,11 +112,13 @@ arch-chroot /mnt echo KEYMAP=jp106 > /etc/vconsole.conf
 # Network Settings
 ################################################################################
 
-# Hostname
+green ""
+green "Hostname..."
 #echo $HOST_NAME > /etc/hostname
 arch-chroot /mnt echo $HOST_NAME > /etc/hostname
 
-# Enable NetworkManager Service
+green ""
+green "Enable NetworkManager Service..."
 #systemctl enable NetworkManager
 arch-chroot /mnt systemctl enable NetworkManager
 
@@ -120,50 +131,49 @@ arch-chroot /mnt systemctl enable NetworkManager
 #pacman -S intel-ucode
 #arch-chroot /mnt pacman -S intel-ucode --noconfirm
 
-# Install grub (for BIOS)
+green ""
+green "Install grub (for BIOS)..."
 #pacman -S grub
 #grub-install --target=i386-pc --recheck /dev/sda
 arch-chroot /mnt pacman -S grub --noconfirm
 arch-chroot /mnt grub-install --target=i386-pc --recheck /dev/sda
 
-# Create GRUB-Setting-File
+green ""
+green "Create grub.cfg..."
 #grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 ################################################################################
 # Root and User Settings
 ################################################################################
-arch-chroot /mnt echo $ROOT_PASSWORD
-arch-chroot /mnt echo $USER_NAME
-arch-chroot /mnt echo $USER_PASSWORD
 
-echo "Set Root Password"
-# Set Root Password
+# デバッグ
+#arch-chroot /mnt echo $ROOT_PASSWORD
+#arch-chroot /mnt echo $USER_NAME
+#arch-chroot /mnt echo $USER_PASSWORD
+
+green ""
+green "Set Root Password..."
 #passwd << __EOF__
 #$ROOT_PASSWORD
 #$ROOT_PASSWORD
 #__EOF__
-
-########################################
-# ここでエラーになっている（<< __EOF__ が問題か？）
-########################################
 arch-chroot /mnt passwd << __EOF__
 $ROOT_PASSWORD
 $ROOT_PASSWORD
 __EOF__
 
-echo "Create User"
-# Create User
+green ""
+green "Create User..."
 #useradd -m $USER_NAME
 arch-chroot /mnt useradd -m $USER_NAME
 
-echo "Set User Password"
-# Set User Password
+green ""
+green "Set User Password..."
 #passwd $USER_NAME << __EOF__
 #$USER_PASSWORD
 #$USER_PASSWORD
 #__EOF__
-
 arch-chroot /mnt passwd $USER_NAME << __EOF__
 $USER_PASSWORD
 $USER_PASSWORD
@@ -172,8 +182,8 @@ __EOF__
 # Install sudo（インストール済み。ベースシステムインストール時か？）
 #pacman -S sudo
 
-echo "Add sudo permission for User"
-# Add sudo permission for User
+green ""
+green "Add sudo permission for User..."
 #sed -i "s/root ALL=(ALL:ALL) ALL/root ALL=(ALL:ALL) ALL\n$USER_NAME ALL=(ALL:ALL) ALL/g" /etc/sudoers
 arch-chroot /mnt sed -i "s/root ALL=(ALL:ALL) ALL/root ALL=(ALL:ALL) ALL\n$USER_NAME ALL=(ALL:ALL) ALL/g" /etc/sudoers
 
@@ -186,8 +196,10 @@ arch-chroot /mnt sed -i "s/root ALL=(ALL:ALL) ALL/root ALL=(ALL:ALL) ALL\n$USER_
 # Exit Root
 #exit
 
-# unmount
+green ""
+green "unmount /mnt..."
 umount -R /mnt
 
-# Shutdown
-echo Type poweroff
+green ""
+green "Install is Complete."
+green "Shutdown. type poweroff"
